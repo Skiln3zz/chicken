11398244fec48449c181780f66a79d88f76e9f0b
BRUART Alexandre <albruart@hotmail.com>
2023-09-27 11:56:56 +0200

    upgrades

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 var clicks = 0;
+var pince = 0;
+var prixpince = 100;
+
 function increment(){
-    clicks = clicks + 1;
-     counter.innerHTML = clicks;
+    if (pince == 0){
+        clicks = clicks + 1;
+        counter.innerHTML = clicks;
+    }
+    else {
+        clicks = clicks + 1 * pince;
+        counter.innerHTML = clicks;
+    }
+}
+
+function buyupgrade(upgrade){
+    if (upgrade == "pince"){
+        if(clicks >= prixpince){
+            clicks = clicks - prixpince;
+            pince++;
+            prixpince = prixpince * 1,5;
+            txtprixpince.innerHTML = prixpince;
+        }
+    }
 }
